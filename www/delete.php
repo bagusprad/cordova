783c294356842e55e72e88dc4f34341b121ad89b
bagusprad <48480695+bagusprad@users.noreply.github.com>
2019-05-13 20:24:24 +0700

    Update delete.php

--- www/delete.php
+++ www/delete.php
@@ -1,9 +1,9 @@
 <?php
  include "db.php";
- if(isset($_GET['id']))
+ if(isset($_GET['id_minuman']))
  {
- $id=$_GET['id'];
- $q=mysqli_query($con,"delete from `course_details` where `id`='$id'");
+ $id_minuman=$_GET['id_minuman'];
+ $q=mysqli_query($con,"delete from `minuman` where `id_minuman`='$id_minuman'");
  if($q)
  echo "success";
  else
